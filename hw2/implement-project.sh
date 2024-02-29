6d39cc07b1f6a912bca1942c6e73717395767ee9
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-02-28 21:04:01 -0500

    wrote implementation script

---
+++ hw2/implement-project.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+# FILENAME: implement-project.sh
+# AUTHOR: Zachary Krepelka
+# DATE: Wednesday, February 28th, 2024
+# ORIGIN: http://github.com/zachary-krepelka/server-side_programming.git
+
+cp users-and-groups.conf /etc/apache2/sites-available/users-and-groups.conf
+cp users-and-groups.py /usr/local/bin/users-and-groups.py
+cp users-and-groups.service /etc/systemd/user/users-and-groups.service
