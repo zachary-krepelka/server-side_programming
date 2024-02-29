5f3fdc0319e0daa51f6b8adc20cbeefdacc76e28
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-02-28 21:25:52 -0500

    revised comment header

--- hw2/implement-project.sh
+++ hw2/implement-project.sh
@@ -3,6 +3,7 @@
 # FILENAME: implement-project.sh
 # AUTHOR: Zachary Krepelka
 # DATE: Wednesday, February 28th, 2024
+# ABOUT: a homework assignment for my server-side programming class
 # ORIGIN: http://github.com/zachary-krepelka/server-side_programming.git
 
 cp users-and-groups.conf /etc/apache2/sites-available/users-and-groups.conf
