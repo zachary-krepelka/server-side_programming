7dfe3a4c005008c90512adc66e9eb79428fcca0f
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2024-04-07 23:57:56 -0400

    revised comment

--- hw6/grade.php
+++ hw6/grade.php
@@ -5,7 +5,7 @@
 // DATE: Sunday, April 7th, 2024
 // ABOUT: a homework assignment for my server-side programming class
 // ORIGIN: https://github.com/zachary-krepelka/server-side_programming.git
-// PATH: /home/zach/github/server-side_programming/hw6/grade.php
+// PATH: /var/www/html/grade.php
 
 function computeLetterGrade($percentage) {
 
